5881cfb33b4e9606ad66b3ea130d46fd9bbb50ea
tareqsifat <mdtareq.me@gmail.com>
2022-11-30 00:54:01 +0600

    ReposityInterface Model Added

---
+++ app/Http/Controllers/CommonController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Interfaces\CommonInterface;
+use App\Repository\CommonRepository;
+use Illuminate\Http\Request;
+
+class CommonController extends Controller
+{
+    private $commonRepository;
+    private $commonInterface;
+
+    public function _construct(CommonRepository $commonRepository, CommonInterface $commonInterface)
+    {
+        $this->$commonInterface = $commonInterface;
+        $this->commonRepository = $commonRepository;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getAllwithoutPagination()
+    {
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Repository/CommonRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repository;
+
+use App\Interfaces\CommonInterface;
+
+class CommonRepository implements CommonInterface
+{
+    
+}
